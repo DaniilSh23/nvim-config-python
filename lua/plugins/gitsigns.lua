faf78699ae71427841b2e21b5be70f6b81992502
DaniilSh23 <danyasevas11@gmail.com>
2024-12-13 17:29:04 +0300

    настройки gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,5 +2,21 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		opt = {},
-	}
+		config = function()
+			require("gitsigns").setup({
+				auto_attach = true,
+				attach_to_untracked = false,
+				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+					use_focus = true,
+				},
+				current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+			})
+		end,
+	},
 }
